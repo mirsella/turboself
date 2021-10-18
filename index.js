117a0a95caa63409991b94e53f4504c8c3e05cd9
mirsella <mirsella@protonmail.com>
2021-10-18 09:57:45 +0200

    bumped timeout for page loading

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ require('dotenv').config();
 (async () => {
   const browser = await puppeteer.launch({headless:true, defaultViewport: {width: 1280, height: 720}});
   const page = await browser.newPage();
-  await page.goto('https://espacenumerique.turbo-self.com/Connexion.aspx', {waitUntil: 'networkidle0', timeout: 60000});
+  await page.setDefaultTimeout(60000);
+  await page.goto('https://espacenumerique.turbo-self.com/Connexion.aspx', {waitUntil: 'networkidle0'});
 
   await page.type('#ctl00_cntForm_txtLogin', process.env.email)
   await page.type('#ctl00_cntForm_txtMotDePasse', process.env.passwd)
   await page.click('#ctl00_cntForm_btnConnexion')
-  await page.waitForSelector('#ctl00_cntForm_UC_collapseMenu_lbtReserver', {timeout: 60000})
+  await page.waitForSelector('#ctl00_cntForm_UC_collapseMenu_lbtReserver')
   await page.click('#ctl00_cntForm_UC_collapseMenu_lbtReserver')
   await page.waitForSelector('#weeknumber_3')
   // await page.$$eval('[id^=weeknumber_]', els => {
@@ -61,7 +62,7 @@ require('dotenv').config();
   await browser.close()
 })()
   .catch(e => {
-    telegramnotif(process.env.TgId, process.env.TgToken, e)
+    telegramnotif(process.env.TgId, process.env.TgToken, 'error turboself' + e)
       .catch(e => {
         fs.writeFileSync('error.txt', e)
       })
