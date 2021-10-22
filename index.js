6caf8cfcacfe487797755f76e36deba4f4207951
mirsella <mirsella@protonmail.com>
2021-10-22 15:54:16 +0200

    added blacklist week config as url

--- index.js
+++ index.js
@@ -1,57 +1,86 @@
 const telegramnotif = require('telegramnotif');
+const axios = require('axios')
 const fs = require('fs-extra');
 const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin());
 require('dotenv').config();
 (async () => {
-  const browser = await puppeteer.launch({headless:true, defaultViewport: {width: 1280, height: 720}});
-  const page = await browser.newPage();
-  await page.setDefaultTimeout(60000);
-  await page.goto('https://espacenumerique.turbo-self.com/Connexion.aspx', {waitUntil: 'networkidle0'});
-
-  await page.type('#ctl00_cntForm_txtLogin', process.env.email)
-  await page.type('#ctl00_cntForm_txtMotDePasse', process.env.passwd)
-  await page.click('#ctl00_cntForm_btnConnexion')
-  await page.waitForSelector('#ctl00_cntForm_UC_collapseMenu_lbtReserver')
-  await page.click('#ctl00_cntForm_UC_collapseMenu_lbtReserver')
-  await page.waitForSelector('#weeknumber_3')
-  // await page.$$eval('[id^=weeknumber_]', els => {
-  //   els.map(el => el.classList.remove('hidden'))
+  // const browser = await puppeteer.launch({headless:true, defaultViewport: {width: 1280, height: 720}});
+  // const page = await browser.newPage();
+  // await page.setDefaultTimeout(60000);
+  // await page.goto('https://espacenumerique.turbo-self.com/Connexion.aspx', {waitUntil: 'networkidle0'});
+  // 
+  // await page.type('#ctl00_cntForm_txtLogin', process.env.email)
+  // await page.type('#ctl00_cntForm_txtMotDePasse', process.env.passwd)
+  // await page.click('#ctl00_cntForm_btnConnexion')
+  // await page.waitForSelector('#ctl00_cntForm_UC_collapseMenu_lbtReserver')
+  // await page.click('#ctl00_cntForm_UC_collapseMenu_lbtReserver')
+  // await page.waitForSelector('#weeknumber_3')
+  // // await page.$$eval('[id^=weeknumber_]', els => {
+  // //   els.map(el => el.classList.remove('hidden'))
+  // // })
+  // const money = parseInt((await page.$eval('.prix', el => el.innerText)).split(' ')[0].replace(',', '.'))
+  // money <= 10 && telegramnotif(process.env.TgId, process.env.TgToken, `today is not reserved ! ${reservation.date}`)
+  // const reservations = await page.$$eval('.day_line', lines => {
+  //   let reservation = []
+  //   for (line of lines) {
+  //     if (line.childElementCount > 1 && ! line.childNodes[1].className.includes('disabled')) {
+  //       // reservation.push(line)
+  //       reservation.push({
+  //         date: line.childNodes[1].innerText,
+  //         reserved: line.childNodes[2].childNodes[3].classList[1] === 'on' ? true : false
+  //       })
+  //     }
+  //   }
+  //   return reservation
   // })
-  const money = parseInt((await page.$eval('.prix', el => el.innerText)).split(' ')[0].replace(',', '.'))
-  money <= 10 && telegramnotif(process.env.TgId, process.env.TgToken, `today is not reserved ! ${reservation.date}`)
-  const reservations = await page.$$eval('.day_line', lines => {
-    let reservation = []
-    for (line of lines) {
-      if (line.childElementCount > 1 && ! line.childNodes[1].className.includes('disabled')) {
-        // reservation.push(line)
-        reservation.push({
-          date: line.childNodes[1].innerText,
-          reserved: line.childNodes[2].childNodes[3].classList[1] === 'on' ? true : false
-        })
-      }
-    }
-    return reservation
-  })
+  // browser.close()
 
   const months = { "JAN.": 0, "FEV.": 1, "MAR.": 2, "AVR.": 3, "MAI.": 4, "JUIN.": 5, "JUIL.": 6, "AOU.": 7, "SEPT.": 8, "OCT.": 9, "NOV.": 10, "DEC.": 11 }
+
+  const fs = require('fs-extra');
+  // await fs.writeJson('reservations.json', reservations)
+  reservations = await fs.readJson('reservations.json')
+
+  // blacklist date
+  nextweekblacklist = []
+  await axios.get(process.env.url)
+  .then(res => {
+    if (res.status === 200) {
+      dateblacklist = res.data
+      d = dateblacklist.split('/')
+      day = d[0]
+      month = d[1] - 1
+      year = d[2]
+      currentyear = new Date().getFullYear()
+      blacklistdate = new Date(currentyear.toString().substring(0,2) + year, month, day)
+      nextweekblacklist = []
+      for (let i = 0; i<7; i+=1) {
+        date = new Date(blacklistdate)
+        date.setDate(date.getDate() + i)
+        nextweekblacklist.push(date.getTime())
+      }
+    }
+  })
+
+  today = new Date()
+  today.setHours(0,0,0,0);
+
+  nextweek = new Date()
+  nextweek.setHours(0,0,0,0);
+  nextweek.setDate(nextweek.getDate()+7)
 
   for (reservation of reservations) {
     d = reservation.date.split(' ')
     date = new Date()
     date.setHours(0,0,0,0);
     date.setDate(parseInt(d[1]))
-    date.setMonth(months[d[2]])
+    date.setMonth(months[d[2].toUpperCase()])
+    console.log(date)
 
-    today = new Date()
-    today.setHours(0,0,0,0);
-
-    nextweek = new Date()
-    nextweek.setHours(0,0,0,0);
-    nextweek.setDate(nextweek.getDate()+7)
-
-    if (! reservation.reserved) {
+    if (! reservation.reserved && ! nextweekblacklist.includes(date.getTime())) {
+      // console.log('not reserved', date, date.toString())
       if (date.getTime() === today.getTime()) {
         telegramnotif(process.env.TgId, process.env.TgToken, `today is not reserved ! ${reservation.date}`)
       } else if (date <= nextweek) {
@@ -59,7 +88,6 @@ require('dotenv').config();
       }
     }
   }
-  await browser.close()
 })()
   .catch(e => {
     telegramnotif(process.env.TgId, process.env.TgToken, 'error turboself' + e)
