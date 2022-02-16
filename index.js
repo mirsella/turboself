ff2dfe2bbd9d3fb7f4a13969e648c6512c74a71c
mirsella <mirsella@protonmail.com>
2022-02-16 10:44:17 +0100

    Wed Feb 16 10:44:17 AM CET 2022

--- index.js
+++ index.js
@@ -6,44 +6,44 @@ const fs = require('fs');
 puppeteer.use(StealthPlugin());
 require('dotenv').config();
 (async () => {
-  // const browser = await puppeteer.launch({headless:true, defaultViewport: {width: 1280, height: 720}});
-  // const page = await browser.newPage();
-  // await page.setDefaultTimeout(60000);
-  // await page.goto('https://espacenumerique.turbo-self.com/Connexion.aspx', {waitUntil: 'networkidle0'})
-  // .catch(err => {
-  //   page.screenshot({path: 'screenshot.png'});
-  //   telegramnotif(process.env.TgId, process.env.TgToken, 'error turboself' + err)
-  // });
-  // 
-  // await page.type('#ctl00_cntForm_txtLogin', process.env.email)
-  // await page.type('#ctl00_cntForm_txtMotDePasse', process.env.passwd)
-  // await page.click('#ctl00_cntForm_btnConnexion')
-  // await page.waitForSelector('#ctl00_cntForm_UC_collapseMenu_lbtReserver')
-  //   .catch(err => {
-  //     page.screenshot({path: 'screenshot.png'});
-  //     telegramnotif(process.env.TgId, process.env.TgToken, 'error turboself' + err)
-  //   });
-  // await page.click('#ctl00_cntForm_UC_collapseMenu_lbtReserver')
-  // await page.waitForSelector('#weeknumber_3')
-  // // await page.$$eval('[id^=weeknumber_]', els => {
-  // //   els.map(el => el.classList.remove('hidden'))
-  // // })
-  // const money = parseFloat((await page.$eval('.prix', el => el.innerText)).split(' ')[0].replace(',', '.'))
-  // money <= 2.56 && telegramnotif(process.env.TgId, process.env.TgToken, `money is low ${money}€`)
-  // const reservations = await page.$$eval('.day_line', lines => {
-  //   let reservation = []
-  //   for (line of lines) {
-  //     if (line.childElementCount > 1 && ! line.childNodes[1].className.includes('disabled')) {
-  //       // reservation.push(line)
-  //       reservation.push({
-  //         date: line.childNodes[1].innerText,
-  //         reserved: line.childNodes[2].childNodes[3].classList[1] === 'on' ? true : false
-  //       })
-  //     }
-  //   }
-  //   return reservation
+  const browser = await puppeteer.launch({headless:true, defaultViewport: {width: 1280, height: 720}});
+  const page = await browser.newPage();
+  await page.setDefaultTimeout(60000);
+  await page.goto('https://espacenumerique.turbo-self.com/Connexion.aspx', {waitUntil: 'networkidle0'})
+  .catch(err => {
+    page.screenshot({path: 'screenshot.png'});
+    telegramnotif(process.env.TgId, process.env.TgToken, 'error turboself' + err)
+  });
+
+  await page.type('#ctl00_cntForm_txtLogin', process.env.email)
+  await page.type('#ctl00_cntForm_txtMotDePasse', process.env.passwd)
+  await page.click('#ctl00_cntForm_btnConnexion')
+  await page.waitForSelector('#ctl00_cntForm_UC_collapseMenu_lbtReserver')
+    .catch(err => {
+      page.screenshot({path: 'screenshot.png'});
+      telegramnotif(process.env.TgId, process.env.TgToken, 'error turboself' + err)
+    });
+  await page.click('#ctl00_cntForm_UC_collapseMenu_lbtReserver')
+  await page.waitForSelector('#weeknumber_3')
+  // await page.$$eval('[id^=weeknumber_]', els => {
+  //   els.map(el => el.classList.remove('hidden'))
   // })
-  // browser.close()
+  const money = parseFloat((await page.$eval('.prix', el => el.innerText)).split(' ')[0].replace(',', '.'))
+  money <= 2.56 && telegramnotif(process.env.TgId, process.env.TgToken, `money is low ${money}€`)
+  const reservations = await page.$$eval('.day_line', lines => {
+    let reservation = []
+    for (line of lines) {
+      if (line.childElementCount > 1 && ! line.childNodes[1].className.includes('disabled')) {
+        // reservation.push(line)
+        reservation.push({
+          date: line.childNodes[1].innerText,
+          reserved: line.childNodes[2].childNodes[3].classList[1] === 'on' ? true : false
+        })
+      }
+    }
+    return reservation
+  })
+  browser.close()
 
   const months = { "JANV.": 0, "FÉVR.": 1, "MAR.": 2, "AVR.": 3, "MAI.": 4, "JUIN.": 5, "JUIL.": 6, "AOU.": 7, "SEPT.": 8, "OCT.": 9, "NOV.": 10, "DÉC.": 11 }
 
@@ -68,7 +68,7 @@ require('dotenv').config();
       }
     }
   })
-  console.log(blacklist)
+  console.log(blacklist.toString())
 
   today = new Date()
   today.setHours(0,0,0,0);
@@ -95,7 +95,7 @@ require('dotenv').config();
   }
 })()
   .catch(e => {
-    console.log(e)
+    console.log('catch', e)
     telegramnotif(process.env.TgId, process.env.TgToken, 'error turboself' + e)
       .catch(e => {
         fs.writeFileSync('error.txt', e)
