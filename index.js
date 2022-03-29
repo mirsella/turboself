305412dd7536d746771b15c2c76bb4c8a57a3468
mirsella <mirsella@protonmail.com>
2022-03-29 10:47:06 +0200

    add delay to login

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ require('dotenv').config();
     telegramnotif(process.env.TgId, process.env.TgToken, 'error turboself' + err)
   });
 
-  await page.type('#ctl00_cntForm_txtLogin', process.env.email)
-  await page.type('#ctl00_cntForm_txtMotDePasse', process.env.passwd)
+  await page.type('#ctl00_cntForm_txtLogin', process.env.email, { delay: 50} )
+  await page.type('#ctl00_cntForm_txtMotDePasse', process.env.passwd, { delay: 50} )
   await page.click('#ctl00_cntForm_btnConnexion')
   await page.waitForSelector('#ctl00_cntForm_UC_collapseMenu_lbtReserver')
     .catch(err => {
